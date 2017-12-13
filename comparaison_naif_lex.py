7b782367ebd66f74ac205bdb8ef17405ae260666
ClaireSalome <claire.c.salome@gmail.com>
2017-12-13 17:27:59 +0100

    Algo naïf et comparaison lexicographique

---
+++ comparaison_naif_lex.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 22 12:26:51 2017
+
+@author: clair
+"""
+
+"""QUESTION 5"""
+#Comparer les complexités des deux algorithmes proposés, et
+#vérifier expérimentalement votre analyse en traçant les courbes des temps
+#d'exécution respectifs des deux algorithmes en fonction de n, sur des ensembles
+#de vecteurs tirés aléatoirement à l'aide de la fonction de la question
+#2. Dans les expérimentations, le nombre de vecteurs variera de n = 200
+#à n = 10000 (par pas de 200 par exemple), et on prendra m = 1000. Pour
+#chaque valeur de n, on fera une moyenne du temps d'exécution sur 50 ensembles
+#tirés aléatoirement.
+
